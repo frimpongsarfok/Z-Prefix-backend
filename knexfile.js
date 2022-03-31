75f5411027a1a40f3cb038a15d0be3b44d31e28b
Sarfo Kantanka Frimpong <frimpongsarfok@gmail.com>
2022-03-31 05:49:41 -0400

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -28,7 +28,8 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {'postgres://legsqufnpqgpqm:3e22c16eb68c3f2f331c1b7ee93ab9138ebebd39004b42a2d0e5e453de4d8878@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d6p9jm15ciuj0q',
+    connection: {
+      connectionString:'postgres://legsqufnpqgpqm:3e22c16eb68c3f2f331c1b7ee93ab9138ebebd39004b42a2d0e5e453de4d8878@ec2-34-231-63-30.compute-1.amazonaws.com:5432/d6p9jm15ciuj0q',
       ssl: { rejectUnauthorized: false },
      },
     pool: {
