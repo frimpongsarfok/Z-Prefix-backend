e030a980fb85e9b87239b8e32644c5f05c1076ee
Sarfo Kantanka Frimpong <frimpongsarfok@gmail.com>
2022-04-07 18:36:33 -0400

    set Cookies

--- routes/index.js
+++ routes/index.js
@@ -57,10 +57,14 @@ router.get('/login', function(req, res, next) {
    
   const {username,password}=req.query;
    login(username,password).then(user=>{
-    res
-    .cookie('username',user.username,{ SameSite: 'none', secure: true, expires: new Date(Date.now() + 900000)})
-    .cookie('password',user.password,{ SameSite: 'none', secure: true, expires: new Date(Date.now() + 900000)})
-   }).catch(err=>res.status(401).json(err))
+    
+        res
+        .clearCookie('username')
+        .clearCookie('password')
+         .cookie('username',user.username,{ expires: new Date(Date.now() + 900000), httpOnly: true })
+        .cookie('password',user.password,{ expires: new Date(Date.now() + 900000), httpOnly: true })
+         .status(200).json(user);
+   }).catch(err=>res.status(404).json({status:401,msg:err.detail}))
   
 });
   
@@ -88,8 +92,8 @@ router.post('/signup', function(req, res, next) {
       .then(rows=>{
       query.password=password;  
       res.status(200)
-       .cookie('username',user.username,{ SameSite: 'none', secure: true, expires: new Date(Date.now() + 900000)})
-        .cookie('password',user.password,{ SameSite: 'none', secure: true, expires: new Date(Date.now() + 900000)})
+       .cookie('username',user.username,{ expires: new Date(Date.now() + 900000), httpOnly: true })
+        .cookie('password',user.password,{ expires: new Date(Date.now() + 900000), httpOnly: true })
       .json(query)
     })
       .catch(err=>res.status(401).json({status:401,msg:err.detail}));
@@ -149,8 +153,13 @@ router.put('/user', upload.single('displayImage'),function(req, res, next) {
         query.password=hash;
         knex('users').update(query).where({username:username})
         .then(rows=>res.status(201)
+<<<<<<< HEAD
          .cookie('username',user.username,{ SameSite: 'none', secure: true, expires: new Date(Date.now() + 900000)})
         .cookie('password',user.password,{ SameSite: 'none', secure: true, expires: new Date(Date.now() + 900000)})
+=======
+         .cookie('username',user.username,{ expires: new Date(Date.now() + 900000), httpOnly: true })
+        .cookie('password',user.password,{ expires: new Date(Date.now() + 900000), httpOnly: true })
+>>>>>>> d271deb (set Cookies)
         .json({status:200,msg:'profile updated successful'}))
         .catch(err=>res.status(401).json({status:401,msg:err.detail}));
       });
